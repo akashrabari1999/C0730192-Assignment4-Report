2514c2284ec81714ba4e582359e367527d06f842
visualstudio <visualstudio@122-PC40>
2019-04-05 21:54:04 -0400

    Section E

--- C0730192/Program.cs
+++ C0730192/Program.cs
@@ -7,7 +7,7 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-namespace C0730192
+namespace Assignment4
 {
     class Program
     {
@@ -15,9 +15,10 @@ namespace C0730192
 
         static void Main(string[] args)
         {
+
             Program p = new Program();
             p.Beowulf = new ArrayList();
-            p.Wordskipper();
+            p.Letters();
             Console.ReadLine();
 
         }
@@ -100,37 +101,63 @@ namespace C0730192
             int f = 0;
             foreach (var line in File.ReadAllLines(@"U:\Users\730192\Beowulf.txt"))
             {
+                f++;
                 if (line.Contains("sea") && line.Contains("fare"))
                 {
-                    f++;
+                    Console.WriteLine(f);
                 }
 
             }
-            Console.WriteLine(f);
         }
         public void Wordskipper()
         {
             int f = 0;
-            int m = 0;
-            int n = 0;
             foreach (var line in File.ReadAllLines(@"U:\Users\730192\Beowulf.txt"))
             {
-                if (line.Contains("fare"))
+                f++;
+                if (line.Contains("fare") && (!line.Contains("war")))
                 {
-                    f++;
+                    Console.WriteLine(f);
                 }
 
             }
-            foreach (var line in File.ReadAllLines(@"U:\Users\730192\Beowulf.txt"))
-            {
-                if (line.Contains("war") && line.Contains("fare"))
-                {
-                    m++;
-                }
 
+        }
+        public void Letters()
+        {
+
+            StreamReader reader = new StreamReader(@"U:\Users\730192\Beowulf.txt");
+            string script = reader.ReadToEnd();
+
+            //find number of letters
+            int numberOfLetters = 0;
+            foreach (char letter in script)
+            {
+                numberOfLetters++;
             }
-            n = f - m;
-            Console.WriteLine(n);
+            var text = script.Trim();
+            int wordCount = 0, index = 0;
+
+            //find number of words
+            while (index < text.Length)
+            {
+                // check if current char is part of a word
+                while (index < text.Length && !char.IsWhiteSpace(text[index]))
+                    index++;
+
+                wordCount++;
+
+                // skip whitespace until next word
+                while (index < text.Length && char.IsWhiteSpace(text[index]))
+                    index++;
+            }
+
+            float val2 = (float)numberOfLetters;
+            float val1 = (float)wordCount;
+
+            //find average
+            float avrg = val2 / val1;
+            Console.WriteLine(avrg);
         }
 
     }
