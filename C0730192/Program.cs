e25387b04750ccdb97319deeee4e0bb28801d1b6
visualstudio <visualstudio@122-PC40>
2019-04-05 21:11:40 -0400

    new code

--- C0730192/Program.cs
+++ C0730192/Program.cs
@@ -4,11 +4,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace C0730192
 {
-
     class Program
     {
         ArrayList Beowulf;
@@ -17,9 +17,9 @@ namespace C0730192
         {
             Program p = new Program();
             p.Beowulf = new ArrayList();
-            p.ReadTextFiles();
-
+            p.Wordskipper();
             Console.ReadLine();
+
         }
         public void Run()
         {
@@ -28,10 +28,10 @@ namespace C0730192
 
         public void ReadTextFiles()
         {
+
             // Read file using StreamReader. Read file line by line
             using (StreamReader file = new StreamReader("Beowulf.txt"))
             {
-
                 int counter = 0;
                 string ln;
 
@@ -41,24 +41,98 @@ namespace C0730192
                     Beowulf.Add(ln);
                 }
                 file.Close();
-                Console.WriteLine("File has {contour) lines.");
+                Console.WriteLine($"File has {counter} lines.");
             }
         }
         public int FindNumberOfBlankSpaces(string line)
         {
-            https://stackoverflow.com/questions/17812566/count-words-and-spaces-in-string-c-sharp
             int countletters = 0;
             int countSpaces = 0;
-
-            foreach (char c in line)
             {
-                if (char.IsLetter(c)) { countletters++; }
-
-                if (char.IsWhiteSpace(c)) { countletters++; }
+                foreach (char c in line)
+                {
+                    if (char.IsLetter(c)) { countletters++; }
+                    if (char.IsWhiteSpace(c)) { countSpaces++; }
+                }
+                return countSpaces;
             }
-            return countSpaces;
+        }
+        public void CountLinesReader()
+        {
+            long lineCounter = 0;
+            using (StreamReader fil = new StreamReader("Beowulf.txt"))
+            {
+                while (fil.ReadLine() != null)
+                {
+                    lineCounter++;
+                }
+                Console.WriteLine(lineCounter);
+            }
+        }
+        public void WordCounter()
+        {
 
+            StreamReader reader = new StreamReader("Beowulf.txt");
+            string script = reader.ReadToEnd();
+
+            var text = script.Trim();
+            int wordCount = 0, index = 0;
+
+            while (index < text.Length)
+            {
+                // check if current char is part of a word
+                while (index < text.Length && !char.IsWhiteSpace(text[index]))
+                    index++;
+
+                wordCount++;
+
+                // skip whitespace until next word
+                while (index < text.Length && char.IsWhiteSpace(text[index]))
+                    index++;
+            }
+
+            Console.WriteLine(wordCount);
+
+
+        }
+        public void Wordfinder()
+        {
+            int f = 0;
+            foreach (var line in File.ReadAllLines("Beowulf.txt"))
+            {
+                if (line.Contains("sea") && line.Contains("fare"))
+                {
+                    f++;
+                }
+
+            }
+            Console.WriteLine(f);
+        }
+        public void Wordskipper()
+        {
+            int f = 0;
+            int m = 0;
+            int n = 0;
+            foreach (var line in File.ReadAllLines("Beowulf.txt"))
+            {
+                if (line.Contains("fare"))
+                {
+                    f++;
+                }
+
+            }
+            foreach (var line in File.ReadAllLines("Beowulf.txt"))
+            {
+                if (line.Contains("war") && line.Contains("fare"))
+                {
+                    m++;
+                }
+
+            }
+            n = f - m;
+            Console.WriteLine(n);
         }
 
     }
+
 }
